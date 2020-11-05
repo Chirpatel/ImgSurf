d648f718583d14b8cd457cd191082bc3202ca04c
Chir <chirpatelss@gmail.com>
2020-11-05 20:22:28 +0530

    Added external Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Image from './components/Image/Image';
 import './App.css';
 //import Image from './components/js/image';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Image/>
     </div>
   );
 }
